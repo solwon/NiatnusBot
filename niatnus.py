df4b5fac2ca409f61517eb135b878cac85ef0b9e
Sol Won <haesol3@naver.com>
2021-08-30 02:16:38 +0900

    test code

--- niatnus.py
+++ niatnus.py
@@ -85,7 +85,8 @@ async def lotto_result():
         else:
             response.add_field(name='어제자 방어구', value=f"{context['a_y_name']}\n{context['a_y_winner']}")
 
-        await app.get_guild(782997633328611438).get_channel(876483410590310440).send(embed=response)
+        # await app.get_guild(782997633328611438).get_channel(876483410590310440).send(embed=response)
+        await app.get_guild(416174233409028096).get_channel(881222372898795580).send(embed=response)
 
 
 app.run(secrets['BOT']['token'])
