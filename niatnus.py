82bacd1dfaebf4c720b601f826aca9c881cc0fc1
Sol Won <haesol3@naver.com>
2021-09-12 22:58:31 +0900

    Update niatnus.py

--- niatnus.py
+++ niatnus.py
@@ -304,7 +304,7 @@ async def 가챠통계(ctx):
     userid = ctx.author.id
     username = ctx.author.display_name
     result = niatnusdb.gacha_stats(userid, username)
-    description = f'```\n{username}님은 이만큼 유네뾰이를 사랑해요!\n+ ------ + ------ + ------ + ------ + ------ + ------ +\n| {"총":<6} | {"1성":<6} | {"2성":<6} | {"3성":<6} | {"4성":<6} | {"5성":<6} |\n| {result.star_1:<6} | {result.star_2:<6} | {result.star_3:<6} | {result.star_4:<6} | {result.star_5:<6} |\n+ ------ + ------ + ------ + ------ + ------ + ------ +\n```'
+    description = f'```\n{username}님은 {result.count}만큼 유네뾰이를 사랑해요!\n+ ----- + ----- + ----- + ----- + ----- +\n| {"1s":<5} | {"2s":<5} | {"3s":<5} | {"4s":<5} | {"5s":<5} |\n| {result.star_1:<5} | {result.star_2:<5} | {result.star_3:<5} | {result.star_4:<5} | {result.star_5:<5} |\n+ ----- + ----- + ----- + ----- + ----- +\n```'
     await ctx.reply(description)
 
 
