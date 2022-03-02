f254567dbbd855cc247ba968c4e3a1a625eac9ad
Sol Won <haesol3@naver.com>
2022-03-02 22:15:52 +0900

    Update maho.py

--- maho.py
+++ maho.py
@@ -14,13 +14,17 @@ async def 마호마호(interaction: Interaction):
 
 @bot.slash_command(guild_ids=[416174233409028096])
 async def 마호티콘(interaction: Interaction, eid: str = SlashOption(name='이름', description='이모티콘 이름')):
+    index = 0
+    if eid[-2] == '~':
+        index = int(eid[-1])
+        eid = eid[:-2]
     emojis = interaction.guild.emojis
     emojis = list(filter(lambda x: x.name == eid, emojis))
     if len(emojis) == 0:
         await interaction.response.send_message('없는 이모티콘입니다.')
     else:
         response = nextcord.Embed()
-        response.set_image(url=emojis[0].url)
+        response.set_image(url=emojis[index].url)
         response.set_author(name=interaction.user.display_name, icon_url=str(interaction.user.avatar))
         await interaction.response.send_message(embed=response)
 
